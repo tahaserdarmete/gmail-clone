337a146222c4352120b9d701a0f0a1954285c2ab
tahaserdarmete <tserdarmete@gmail.com>
2025-09-09 17:39:41 +0300

    project complated

---
+++ js/main.js
@@ -0,0 +1,129 @@
+import {uiElement, renderMails, updateMail} from "./ui.js";
+
+const mails = JSON.parse(localStorage.getItem("mails")) || [];
+
+document.addEventListener("DOMContentLoaded", () => {
+  renderMails(uiElement.mailArea, mails);
+});
+
+uiElement.createBtn.addEventListener("click", () => {
+  uiElement.modal.classList.add("open");
+});
+
+uiElement.closeBtn.addEventListener("click", () => {
+  uiElement.modal.classList.remove("open");
+});
+
+uiElement.menuBtn.addEventListener("click", () => {
+  uiElement.leftAside.classList.toggle("hide");
+});
+
+window.addEventListener("resize", (e) => {
+  const screenWidth = e.target.innerWidth;
+
+  if (screenWidth < 1100) {
+    uiElement.leftAside.classList.add("hide");
+  } else {
+    uiElement.leftAside.classList.remove("hide");
+  }
+});
+
+uiElement.form.addEventListener("submit", (e) => {
+  e.preventDefault();
+
+  const reciver = e.target[0].value;
+  const title = e.target[1].value;
+  const message = e.target[2].value;
+
+  if (!reciver && !title && !message) {
+    Toastify({
+      text: "Formu Doldurunuz",
+      duration: 3000,
+      destination: "https://github.com/apvarun/toastify-js",
+      newWindow: true,
+      close: true,
+      gravity: "top", // `top` or `bottom`
+      position: "right", // `left`, `center` or `right`
+      stopOnFocus: true, // Prevents dismissing of toast on hover
+      style: {
+        background: "linear-gradient(to right, red, #464645ff)",
+        borderRadius: "10px",
+      },
+      onClick: function () {}, // Callback after click
+    }).showToast();
+    return;
+  }
+
+  const newMail = {
+    id: new Date().getTime(),
+    reciver,
+    title,
+    message,
+    stared: false,
+    date: new Date().toLocaleDateString("tr", {
+      day: "2-digit",
+      month: "long",
+    }),
+  };
+
+  mails.unshift(newMail);
+
+  localStorage.setItem("mails", JSON.stringify(mails));
+
+  e.target.reset();
+
+  uiElement.modal.classList.remove("open");
+
+  Toastify({
+    text: "Mail Gönderildi",
+    duration: 3000,
+    destination: "https://github.com/apvarun/toastify-js",
+    newWindow: true,
+    close: true,
+    gravity: "top", // `top` or `bottom`
+    position: "right", // `left`, `center` or `right`
+    stopOnFocus: true, // Prevents dismissing of toast on hover
+    style: {
+      background: "linear-gradient(to right, #00b09b, #96c93d)",
+      borderRadius: "10px",
+    },
+    onClick: function () {}, // Callback after click
+  }).showToast();
+
+  renderMails(uiElement.mailArea, mails);
+});
+
+uiElement.mailArea.addEventListener("click", updateMail);
+
+uiElement.navigation.addEventListener("click", (e) => {
+  const clickCategory = e.target.closest("a");
+  const links = uiElement.navigation.querySelectorAll("a");
+
+  links.forEach((link) => link.classList.remove("active"));
+
+  clickCategory.classList.add("active");
+
+  const categoryName = clickCategory.innerText.trim();
+
+  if (categoryName === "Yıldızlananlar") {
+    const staredMails = mails.filter((mail) => mail.stared === true);
+
+    renderMails(uiElement.mailArea, staredMails);
+  } else {
+    renderMails(uiElement.mailArea, mails);
+  }
+});
+
+uiElement.searchForm.addEventListener("submit", (e) => {
+  e.preventDefault();
+
+  const query = e.target[1].value;
+
+  const filteredMails = mails.filter((i) =>
+    i.reciver.toLowerCase().includes(query.toLowerCase())
+  );
+
+  renderMails(uiElement.mailArea, filteredMails);
+});
+
+export {mails};
---
+++ js/ui.js
@@ -0,0 +1,98 @@
+import {mails} from "./main.js";
+
+const uiElement = {
+  menuBtn: document.querySelector("#menu-btn"),
+  modal: document.querySelector(".modal-wrapper"),
+  closeBtn: document.querySelector("#close-btn"),
+  navigation: document.querySelector(".left-aside-middle"),
+  createBtn: document.querySelector(".create"),
+  leftAside: document.querySelector(".left-aside"),
+  form: document.querySelector("#mail-form"),
+  mailArea: document.querySelector(".mails-area"),
+  searchForm: document.querySelector(".search-form"),
+};
+
+const renderMails = (outlet, data) => {
+  outlet.innerHTML = data
+    .map(
+      (mail) =>
+        `
+     <div class="mail" data-id="${mail.id}">
+            <div class="left">
+              <input type="checkbox" />
+              <i class="bi bi-star${mail.stared ? "-fill" : ""} "></i>
+              <span>${mail.reciver}</span>
+            </div>
+
+            <div class="center">
+              <p class="mail-title">${mail.title}</p>
+              <p class="mail-desc">${mail.message}</p>
+            </div>
+
+            <div class="right">
+              <p class="mail-date">${mail.date}</p>
+              <div class="delete">
+                <i class="bi bi-trash-fill"></i>
+              </div>
+            </div>
+          </div>
+    `
+    )
+    .join("");
+};
+
+const updateMail = (e) => {
+  if (e.target.classList.contains("bi-trash-fill")) {
+    const res = confirm("Silme işlemini onaylıyor musunuz?");
+
+    if (res) {
+      const mail = e.target.closest(".mail");
+
+      const mailId = +mail.dataset.id;
+
+      const filteredMails = mails.filter((mail) => mail.id != mailId);
+
+      localStorage.setItem("mails", JSON.stringify(filteredMails));
+
+      mail.remove();
+
+      Toastify({
+        text: "Mail Silindi",
+        duration: 3000,
+        destination: "https://github.com/apvarun/toastify-js",
+        newWindow: true,
+        close: true,
+        gravity: "top", // `top` or `bottom`
+        position: "right", // `left`, `center` or `right`
+        stopOnFocus: true, // Prevents dismissing of toast on hover
+        style: {
+          background: "linear-gradient(to right, tomato, #eaf097ff)",
+        },
+        onClick: function () {}, // Callback after click
+      }).showToast();
+    }
+  }
+
+  if (
+    e.target.classList.contains("bi-star") ||
+    e.target.classList.contains("bi-star-fill")
+  ) {
+    const mail = e.target.closest(".mail");
+
+    const mailId = parseInt(mail.dataset.id);
+
+    const foundedMail = mails.find((i) => i.id == mailId);
+
+    const updateMail = {...foundedMail, stared: !foundedMail.stared};
+
+    const index = mails.findIndex((i) => i.id == mailId);
+
+    mails[index] = updateMail;
+
+    localStorage.setItem("mails", JSON.stringify(mails));
+
+    renderMails(uiElement.mailArea, mails);
+  }
+};
+
+export {uiElement, renderMails, updateMail};
